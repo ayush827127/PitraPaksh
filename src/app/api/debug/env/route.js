a378e965c951cc5355bc47c422490b027194485e
Ayush Gupta <ayush385361@gmail.com>
2026-07-15 21:28:24 +0530

    db testing

---
+++ src/app/api/debug/env/route.js
@@ -0,0 +1,27 @@
+import { NextResponse } from 'next/server'
+import connectDB from '../../../../lib/db/connect'
+
+export async function GET() {
+  const envPresence = {
+    MONGODB_URI: !!process.env.MONGODB_URI,
+    AUTH_SECRET: !!process.env.AUTH_SECRET,
+    AUTH_URL: process.env.AUTH_URL || null,
+    GOOGLE_CLIENT_ID: !!process.env.GOOGLE_CLIENT_ID,
+    GOOGLE_CLIENT_SECRET: !!process.env.GOOGLE_CLIENT_SECRET,
+    RAZORPAY_KEY_ID: !!process.env.RAZORPAY_KEY_ID,
+    RAZORPAY_KEY_SECRET: !!process.env.RAZORPAY_KEY_SECRET,
+    NEXT_PUBLIC_RAZORPAY_KEY_ID: !!process.env.NEXT_PUBLIC_RAZORPAY_KEY_ID,
+    ADMIN_SESSION_SECRET: !!process.env.ADMIN_SESSION_SECRET,
+    RAZORPAY_WEBHOOK_SECRET: !!process.env.RAZORPAY_WEBHOOK_SECRET,
+  }
+
+  let mongoStatus = 'not attempted'
+  try {
+    await connectDB()
+    mongoStatus = 'connected'
+  } catch (error) {
+    mongoStatus = `failed: ${error.message}`
+  }
+
+  return NextResponse.json({ envPresence, mongoStatus, nodeEnv: process.env.NODE_ENV })
+}
